2fd1fd47d63a09600e8329c948d70b8e2f383ee6
Noelia Hernández Domínguez <chispa1992@gmail.com>
2018-02-05 18:39:58 +0000

    Actividad 1 Tema 4.

---
+++ trim2/u4/a1/README.md
@@ -0,0 +1,49 @@
+___
+
+# **Actividad Triggers En MySQL.**
+
+---
+
+Creamos una base de datos llamada banco.
+
+![imagen01](./images/01.png)
+
+Dentro de banco, creamos una tabla llamada cuentas que va a tener dos campos, número de cuenta (entero y clave primaria) y saldo (10 partes enteras y 2 partes decimales).
+
+![imagen02](./images/02.png)
+
+Creamos un trigger que guarde en una variable, llamada saldo, el acumulativo de los valores insertados en cada una de las tuplas.
+
+![imagen03](./images/03.png)
+
+![imagen04](./images/04.png)
+
+![imagen05](./images/05.png)
+
+Insertamos valores y comprobamos el total de saldo insertado con un select.
+
+![imagen06](./images/06.png)
+
+![imagen07](./images/07.png)
+
+![imagen08](./images/08.png)
+
+Creamos también un trigger que reste cuando eliminamos una tupla.
+
+![imagen09](./images/09.png)
+
+![imagen10](./images/10.png)
+
+Comprobamos el total del saldo después de los borrados.
+
+![imagen11](./images/11.png)
+
+---
+
+# **Archivo .sql.**
+
+El archivo .sql de esta práctica es el siguiente.
+
+[Archivo banco.sql](https://github.com/NoeClariNista/ade1718/blob/master/trim2/u4/a1/banco.sql)
+
+---
---
+++ trim2/u4/a1/banco.sql
@@ -0,0 +1,52 @@
+# Actividad Triggers en MySQL.
+
+# Crear una base de datos llamada Banco.
+
+create database banco;
+
+use banco;
+
+# Dentro de Banco, crear una tabla llamada cuentas que va a tener dos campos,
+# numero de cuenta (entero y clave primaria) y saldo (10 partes enteras y 2 partes decimales).
+
+CREATE TABLE cuentas(
+	numero_de_cuenta int,
+    saldo decimal(10, 2),
+    PRIMARY KEY(numero_de_cuenta)
+    ) ENGINE = InnoDB;
+
+SET @saldo=0;
+
+select @saldo;
+
+# Crear un trigger que guarde en una variable, llamada saldo, el acumulativo de los
+# valores insertados en cada una de las tuplas.
+
+CREATE TRIGGER insertar_saldo_acumulativo AFTER INSERT ON cuentas
+FOR EACH ROW
+SET @saldo=@saldo+NEW.saldo;
+
+# Insertar valores y comprobar el total de saldo insertado con un select.
+
+INSERT INTO cuentas (numero_de_cuenta, saldo) VALUES
+(123456789,100),
+(234567891,200),
+(345678912,300),
+(456789123,400),
+(567891234,500);
+
+select * from cuentas;
+
+select @saldo;
+
+# Crear también un trigger que reste cuando eliminamos una tupla. 
+
+CREATE TRIGGER eliminar_resta_saldo AFTER DELETE ON cuentas
+FOR EACH ROW
+SET @saldo=@saldo-OLD.saldo;
+
+delete from cuentas where numero_de_cuenta=123456789 and saldo=100;
+
+# Comprobar el total del saldo después de los borrados.
+
+select @saldo;
